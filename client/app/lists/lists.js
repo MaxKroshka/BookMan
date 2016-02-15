1edbc2c9854762c7af5e7766af7c9667da2e6564
Max Kroshka <max.kroshka@gmail.com>
2016-02-15 14:43:21 -0800

    Add new bookmark list input field and button and toggle function

--- client/app/lists/lists.js
+++ client/app/lists/lists.js
@@ -3,5 +3,13 @@ angular.module('bookman.lists', [])
 .controller('ListsCtrl', function ($scope) {
   $scope.data = {};
   $scope.data.lists = [{name: 'Learning'},{name: 'Info'},{name: 'Learning'},{name: 'Info'}];
+  $scope.showInput = false;
 
+  $scope.addList = function(){
+    console.log('Added list!');
+  };
+
+  $scope.toggleInput = function(){
+    $scope.showInput = !$scope.showInput;
+  };
 });
