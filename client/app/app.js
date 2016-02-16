9a6e33fee481a724615234afb7ba2746d87fd90f
Max Kroshka <max.kroshka@gmail.com>
2016-02-16 11:52:07 -0800

    Add multiple routes, location and activeTab rootScope variables

--- client/app/app.js
+++ client/app/app.js
@@ -1,13 +1,31 @@
 angular.module('bookman', [
-  'bookman.services',
-  'bookman.links',
-  'bookman.lists',
-  'ngRoute'
-])
-.config(function ($routeProvider, $httpProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'app/links/links.html',
-      controller: 'LinksController'
+    'bookman.services',
+    'bookman.links',
+    'bookman.lists',
+    'ngRoute'
+  ])
+  .config(function($routeProvider, $httpProvider) {
+    $routeProvider
+      .when('/allLinks', {
+        templateUrl: 'app/links/allLinks.html',
+        controller: 'LinksController'
+      })
+      .when('/bookmarks', {
+        templateUrl: 'app/links/bookmarks.html',
+        controller: 'LinksController'
+      })
+      .when('/readingList', {
+        templateUrl: 'app/links/readingList.html',
+        controller: 'LinksController'
+      })
+      .when('/', {
+        templateUrl: 'app/links/links.html',
+        controller: 'LinksController'
+      });
+  })
+  .run(function($rootScope, $location) {
+    $rootScope.activeTab = 'bookmarks';
+    $rootScope.$on('$routeChangeStart', function(evt, next, current) {
+      $rootScope.location = $location.path();
+    });
   });
-});
